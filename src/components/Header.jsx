5b0b8494feb15c6a215737033acfa179a302f83a
Takumi <fd3s.bcnr33@gmail.com>
2024-02-19 04:58:08 +0900

    2_4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { useCookies } from "react-cookie";
-import { useSelector, useDispatch } from "react-redux/es/exports";
-import { useHistory } from "react-router-dom";
-import  { signOut } from "../authSlice";
-import "./header.css";
+// import { useCookies } from "react-cookie";
+import { useSelector, useDispatch } from "react-redux";
+// 変更
+import { useNavigate } from "react-router-dom";
+import { signOut } from "../authSlice";
+import "./header.scss";
 
 export const Header = () => {
-  const auth = useSelector((state) => state.auth.isSignIn)
+  const auth = useSelector((state) => state.auth.isSignIn);
   const dispatch = useDispatch();
-  const history = useHistory();
-  const [cookies, setCookie, removeCookie] = useCookies();
+  // 変更
+  const navigation = useNavigate();
+  // const [cookies, setCookie, removeCookie] = useCookies();
   const handleSignOut = () => {
     dispatch(signOut());
-    removeCookie("token");
-    history.push("/signin");
-  }
+    // removeCookie("token");
+    // 変更
+    navigation("/signin");
+  };
 
   return (
     <header className="header">
       <h1>Todoアプリ</h1>
-      {auth ? <button onClick={handleSignOut} className="sign-out-button">サインアウト</button> : <></>}
+      {auth ? (
+        <button onClick={handleSignOut} className="sign-out-button">
+          サインアウト
+        </button>
+      ) : (
+        <></>
+      )}
     </header>
-  )
-}
+  );
+};
+
+export default Header;
